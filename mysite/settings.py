02530dbfade6e0df731618e134a941bd7cea9ef4
Paul Gathondu <aulgondu@gmail.com>
2023-05-02 20:05:12 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'countdown-clock/dist/static')
+    os.path.join(BASE_DIR, 'frontend/dist/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
