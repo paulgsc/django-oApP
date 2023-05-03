479c63f757b7bc04e382ea00fb7d657c973335d5
Paul Gathondu <aulgondu@gmail.com>
2023-05-02 21:42:47 -0700

    dd

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://website1.firebaseapp.com",
+    "https://website1.web.app",
+    "https://website1.firebaseio.com"
+]
+
 
 ROOT_URLCONF = 'mysite.urls'
 
